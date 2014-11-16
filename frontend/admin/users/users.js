a7c3087a2a3541ebf1b00f49a844e47c50f071d2
TakesxiSximada <takesxi.sximada@gmail.com>
2014-11-16 14:12:34 +0900

    modified user management page

--- frontend/admin/users/users.js
+++ frontend/admin/users/users.js
@@ -6,32 +6,71 @@
     if (exports.baast.users){
         return;
     }
+    var notification = (function(type, title, message){
+        new PNotify({
+            title: title,
+            text: message,
+            type: type,
+            nonblock: {
+                nonblock: true
+            },
+            before_close: function(PNotify){
+                // You can access the notice's options with this. It is read only.
+                //PNotify.options.text;
+
+                // You can change the notice's options after the timer like this:
+                PNotify.update({
+                    title: PNotify.options.title+" - Enjoy your Stay",
+                    before_close: null
+                });
+                PNotify.queueRemove();
+                return false;
+            }
+        });
+    });
 
     var users = {
-        UserCreator: function (selector){
-            var info = function(type, title, message){
-                new PNotify({
-                    title: title,
-                    text: message,
-                    type: type,
-                    nonblock: {
-                        nonblock: true
-                    },
-                    before_close: function(PNotify){
-                        // You can access the notice's options with this. It is read only.
-                        //PNotify.options.text;
-
-                        // You can change the notice's options after the timer like this:
-                        PNotify.update({
-                            title: PNotify.options.title+" - Enjoy your Stay",
-                            before_close: null
+        UserCollector: function (selector){
+            var db = {
+                'users': [],
+                'count': 0
+            }
+            var app = new Vue({
+                el: selector,
+                data: db,
+                created: function(){
+                    this.$watch('users', function (){
+                        this.count = this.users.length;
+                    });
+                    this.update(db.users);
+                },
+                methods: {
+                    update: function(users){
+                        $.ajax({
+                            'url': '/api/users',
+                            'method': 'GET',
+                            'dataType': 'json',
+                            'beforeSending': function (xhr, settings){
+                                console.log('getting user collection...');
+                            },
+                            'complete': function (xhr, status){
+                                console.log(xhr);
+                            },
+                            'success': function (res, status, xhr){
+                                $.each(res, function(ii, user){
+                                    users.push(user);
+                                });
+                            },
+                            'error': function (res, status, xhr){
+                                notification('error', 'ユーザを取得できませんでした', res.statusText);
+                            }
                         });
-                        PNotify.queueRemove();
-                        return false;
                     }
-                });
-            };
-
+                }
+            });
+            return app;
+        },
+        UserCreator: function (selector){
             var app = new Vue({
                 el: selector,
                 created: function (){
@@ -50,10 +89,13 @@
                                 console.log(xhr);
                             },
                             'success': function (res, status, xhr){
-                                info('success', 'ユーザを作成しました', '');
+                                notification('success', 'ユーザを作成しました', '');
+                                setTimeout(function (){
+                                    location.href='/admin/users';
+                                }, 3000, true);
                             },
                             'error': function (res, status, xhr){
-                                info('error', 'ユーザを作成できませんでした', res.statusText);
+                                notification('error', 'ユーザを作成できませんでした', res.statusText);
                             }
                         });
                     }
