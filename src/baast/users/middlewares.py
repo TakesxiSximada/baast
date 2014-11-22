6035af3326bfeb4661a5f716cd73124a561892f7
TakesxiSximada <takesxi.sximada@gmail.com>
2014-11-22 11:45:46 +0900

    :put_litter_in_its_place:

--- src/baast/users/middlewares.py
+++ src/baast/users/middlewares.py
@@ -3,6 +3,8 @@ from sandstorm.middlewares.core import ViewConfig
 from sandstorm.middlewares.validators import ValidationMiddleware
 from sandstorm.middlewares.transaction import TransactionMiddleware
 from sandstorm.middlewares.responses import PyramidHTTPExceptionMiddleware
+
+
 view_config = ViewConfig(
     PyramidHTTPExceptionMiddleware,
     TransactionMiddleware,
