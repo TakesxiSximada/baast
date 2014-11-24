75a58ddc3d30cfa7424ead6464ad0c1c2529f354
TakesxiSximada <takesxi.sximada@gmail.com>
2014-11-24 12:15:10 +0900

    Added TeadownOptionalMiddleware class
    Do not run the teardown If an exception is thrown.

--- src/baast/users/middlewares.py
+++ src/baast/users/middlewares.py
@@ -4,7 +4,9 @@ from pyramid.httpexceptions import (
     )
 
 from sandstorm.middlewares.core import ViewConfig
-from sandstorm.middlewares.validators import Middleware
+from sandstorm.middlewares.validators import (
+    TeadownOptionalMiddleware,
+    )
 from sandstorm.middlewares.validators import ValidationMiddleware
 from sandstorm.middlewares.transaction import TransactionMiddleware
 from sandstorm.middlewares.responses import PyramidHTTPExceptionMiddleware
@@ -12,15 +14,45 @@ from sandstorm.middlewares.responses import PyramidHTTPExceptionMiddleware
 from .managers import UserManager
 
 
-class AuthenticationMiddleware(Middleware):
+class Authenticator(object):
+    pass
+
+
+class BaastAuthenticator(Authenticator):
+
+    @property
+    def auth_url(self):
+        return ''
+
+    @property
+    def user(self):
+        pass
+
+
+class AuthenticationMiddleware(TeadownOptionalMiddleware):
     keywords = {
-        'auth': False,
+        'auth': 'baast',
         'authentication_key': 'user_id',
         }
+
+    def __init__(self, *args, **kwds):
+        self.redirect_url = ''
+        self.auth_url = ''
+        super().__init__(*args, **kwds)
 
     def setup(self, handler):
         user_id = handler.get_secure_cookie(self.authentication_key)
         if self.auth:
+            try:
+                self.redirect_url = handler.get_arguments('redirect_url')[0]
+            except IndexError:
+                pass
+
+            try:
+                self.auth_url = handler.get_arguments('auth_url')[0]
+            except IndexError:
+                pass
+
             try:
                 user_id = int(user_id)
             except (ValueError, TypeError):
@@ -28,10 +60,17 @@ class AuthenticationMiddleware(Middleware):
             manager = UserManager()
             users = manager.get([user_id])
             if self.auth and len(users) != 1:
-                raise HTTPUnauthorized()
+                if self.auth_url:
+                    handler.redirect(self.redirect_url)
+                else:
+                    raise HTTPUnauthorized()
             else:
                 user = users[0]
                 handler.get_current_user = lambda *args, **kwds: user
+
+    def teardown(self, handler):
+        if self.redirect_url:
+            handler.redirect(self.redirect_url)
 
 
 view_config = ViewConfig(
